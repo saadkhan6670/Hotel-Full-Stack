a1109fa383ab378b5a76a0943b8d50bcf34a7709
saadkhan6670 <saad.khan6670@gmail.com>
2017-09-06 15:04:11 +0500

    intial

--- hotel-backend/hotel/controller.js
+++ hotel-backend/hotel/controller.js
@@ -5,12 +5,19 @@ var boom = require('boom');
 
 exports.SearchAll = function (req,res) {
     console.log("data hit")
-    Hotels.find({},function (err,data) {
-        if(err){
-            res.send(err);
-        }
-        else{
-       res.send(data);
-        }
-})
+
+    Hotels.find({name : {$regex: new RegExp('.*' + search.toLowerCase()+'.*', 'i')},name : {$regex: new RegExp('.*' + search.toUpperCase()+'.*','i')},is_active:true},
+    function(err,data) { 
+        console.log(data); 
+    });
+
+
+//     Hotels.find({},function (err,data) {
+//         if(err){
+//             res.send(err);
+//         }
+//         else{
+//        res.send(data);
+//         }
+// })
 }
