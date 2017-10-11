930e7d2a4f72098c48f7a2e29526946c061daaca
saadkhan6670 <saad.khan6670@gmail.com>
2017-10-11 16:44:07 +0500

    work

--- hotel-frontend/src/MatchedHotels.js
+++ hotel-frontend/src/MatchedHotels.js
@@ -14,7 +14,8 @@ class MatchedHotels extends Component {
         this.Hotel = this.props.Hotel;
         this.state = {
             Hotels_data : [],
-            activePage: 1
+            activePage: 1,
+            itemsCountPerPage : 40
         };
     }
 
@@ -26,70 +27,85 @@ class MatchedHotels extends Component {
 }
 
 handlePageChange = (pageNumber) =>  {
-    console.log(`active page is ${pageNumber}`);
+   
     this.setState({activePage: pageNumber});
   }
 
 
     render() {
+         // Logic for displaying current todos
+         const indexOfLastHotel = this.state.activePage * this.state.itemsCountPerPage;
+         const indexOfFirstHotel = indexOfLastHotel - this.state.itemsCountPerPage;
+         const currentHotels = this.state.Hotels_data.slice(indexOfFirstHotel, indexOfLastHotel);
+
         return (<div>
-          <Pagination
+
+        <Pagination
         firstPageText={<i className='glyphicon glyphicon-chevron-left'/>}
         lastPageText={<i className='glyphicon glyphicon-chevron-right'/>}
         prevPageText={<i className='glyphicon glyphicon-menu-left'/>}
         nextPageText={<i className='glyphicon glyphicon-menu-right'/>}
           activePage={this.state.activePage}
-          itemsCountPerPage={10}
+          itemsCountPerPage={this.state.itemsCountPerPage}
           totalItemsCount={this.state.Hotels_data.length}
           pageRangeDisplayed={5}
           onChange={this.handlePageChange}
         />
 
-        {this.state.Hotels_data.map(data => {
-           return (
+        { currentHotels.map(data => {
+        return (
 <div className="container">
-    <div className="row">
-    	<div className="col-md-12">
-			<div className="col-sm-6 col-md-6">
-				<div className="thumbnail" >
-					<img src={data.image.map( img => {return img.url})} className="img-responsive" alt="ahsan ki iamge" />
-                    
-					<div className="caption">
-                    <Rater total={5} rating={data.rating.map(rating => {return rating.value})} interactive={false} />
-						<div className="row">
-							<div className="col-md-6 col-xs-6">
-								<h3>{data.summary.hotelName}</h3>
-							</div>
-							<div className="col-md-6 col-xs-6 price">
-							
-							</div>
-						</div>
-						<p>{data.location.address}</p>
-						<div className="row">
-							<div className="col-md-6">
-									<h3> <label>{Math.floor(data.summary.lowRate)}</label> SAR</h3>
-							</div>
-							<div className="col-md-12">
-								<a className="btn btn-warning btn-product"> Select</a></div>
-						</div>
+ <div className="row">
+     <div className="col-md-12">
+         <div className="col-sm-6 col-md-6">
+             <div className="thumbnail" >
+                 <img src={data.image.map( img => {return img.url})} className="img-responsive" alt="ahsan ki iamge" />
+                 
+                 <div className="caption">
+                 <Rater total={5} rating={data.rating.map(rating => {return rating.value})} interactive={false} />
+                     <div className="row">
+                         <div className="col-md-6 col-xs-6">
+                             <h3>{data.summary.hotelName}</h3>
+                         </div>
+                         <div className="col-md-6 col-xs-6 price">
+                         
+                         </div>
+                     </div>
+                     <p>{data.location.address}</p>
+                     <div className="row">
+                         <div className="col-md-6">
+                                 <h3> <label>{Math.floor(data.summary.lowRate)}</label> SAR</h3>
+                         </div>
+                         <div className="col-md-12">
+                             <a className="btn btn-warning btn-product"> Select</a></div>
+                     </div>
 
-						<p> </p>
-					</div>
-				</div>
-			</div>
-            
-        </div> 
+                     <p> </p>
+                 </div>
+             </div>
+         </div>
+         
+     </div> 
 
-	</div>
+ </div>
 </div>)
-        })}
-
+     })}
       
         </div>
         )
     }
-}
+};
+
+
+export class HotelData extends Component {
+    render() {
+        return (<div>
+    
+    </div> )}
+};
+
 
 //https://github.com/AdeleD/react-paginate/blob/master/demo/js/demo.js 
+//https://codepen.io/PiotrBerebecki/pen/pEYPbY?editors=0010
 
 export default MatchedHotels;
